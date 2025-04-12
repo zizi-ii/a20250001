04a97ce723883dca26a7deaa44cd674e3a4a2a1c
Lee, Jiwoo <zizii23027@gmail.com>
2025-04-12 21:35:06 +0900

    five

--- src/a20250001/World.java
+++ src/a20250001/World.java
@@ -3,4 +3,8 @@ public class World {
     public static void run() {
         System.out.println("20250001");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
